3383f0ac3b358c949a0f2b696a52f1c53d147e48
AbidDhankwala123 <104614734+AbidDhankwala123@users.noreply.github.com>
2023-12-27 21:53:22 +0530

    backend completed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const errorHandler  = require('./middleware/errorMiddleware');
-require("dotenv").config();
+require("dotenv").config({path:".env"});
 const cors = require("cors");
 
 const app = express();
